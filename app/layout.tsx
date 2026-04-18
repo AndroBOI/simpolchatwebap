4b3aae6983db6d80d5342c3d11bcf1aa8a2822b9
andrew <andrewjovellanos48@gmail.com>
2026-04-18 15:13:20 +0800

    socket connectio0n

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import "./globals.css";
-
+import { AuthButton } from "@/components/auth-button";
+import { Suspense } from "react";
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : "http://localhost:3000";
@@ -33,6 +34,9 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <Suspense fallback={null}>
+            <AuthButton />
+          </Suspense>
           {children}
         </ThemeProvider>
       </body>
